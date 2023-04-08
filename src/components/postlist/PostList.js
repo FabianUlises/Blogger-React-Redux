378832e46414c88593f9f3ff0c492f72d4d76f0e
FabianUlises <ulisesfab.car@outlook.com>
2023-04-07 21:53:24 -0700

    Looping through data to render post component

--- src/components/postlist/PostList.js
+++ src/components/postlist/PostList.js
@@ -11,13 +11,22 @@ const PostsList = () => {
   const dispatch = useDispatch();
   // Useeffect
   useEffect(() => {
+    // Fetching data using redux
     dispatch(fetchPosts());
   }, []);
+  // Selecting data from store
+  const { posts, loading, error} = useSelector((state) => {
+    return state.posts;
+  });
+  // Looping through posts and creating post component with data pass down as props
+  const displayPosts = posts.map((post) => (
+    <Post title={post.title} content={post.body} key={post.id} />
+  ));
   return (
     <section className='postlist-container'>
       <h2 className='section-title'>Total Posts 100</h2>
       <div className='posts-container'>
-        <Post />
+        {displayPosts}
       </div>
     </section>
   );

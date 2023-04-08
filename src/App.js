bcee63e3b96cca50fd62ebfd6eb56dbc381f4c7d
FabianUlises <ulisesfab.car@outlook.com>
2023-04-07 17:42:52 -0700

    Setting user input state and created function to handle form submit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,6 @@ function App() {
       <SearchPost />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/searchpost/SearchPost.js
+++ src/components/searchpost/SearchPost.js
@@ -1,20 +1,29 @@
 // Dependencies
-import React from 'react'
-// Styles
-import styles from './searchpost.css';
+import React, { useState } from 'react';
 
 const SearchPost = () => {
-  return (
-    <div className='searchForm'>
-        <h1 className='searchForm__title'>Blogger <span>w/</span> React<span>&</span>Redux</h1>
-        <p className='searchForm__content'>This is a project made with React Redux that fetches data from api</p>
-        <div className='searchForm__form-container'>
-            <form className='searchForm__form'>
-                <input type='text' placeholder='search for a post' />
-                <button type='submit' className='form-btn'>Search</button>
-            </form>
+    // State
+    const [input, setInput] = useState('');
+    // Handle user input and set it to state
+    const handleInput = (e) => {
+        // Updaing state with user input
+        setInput(e.target.value);
+    };
+    // Handle form submit
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+    return (
+        <div className='searchForm'>
+            <h1 className='searchForm__title'>Blogger <span>w/</span> React<span>&</span>Redux</h1>
+            <p className='searchForm__content'>This is a project made with React Redux that fetches data from api</p>
+            <div className='searchForm__form-container'>
+                <form className='searchForm__form' onSubmit={handleSubmit} >
+                    <input type='text' placeholder='search for a post' onChange={handleInput} />
+                    <button type='submit' className='form-btn'>Search</button>
+                </form>
+            </div>
         </div>
-    </div>
   );
 };
 

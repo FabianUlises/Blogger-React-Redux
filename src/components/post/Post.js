dfbe996007bfd266ee299bb466fa3a96d56363a0
FabianUlises <ulisesfab.car@outlook.com>
2023-04-08 00:24:36 -0700

    Accessing data in post passed down from redux store as props

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -2,10 +2,11 @@
 import React from 'react';
 // Styles
 import styles from './post.css';
-const Post = () => {
+const Post = (props) => {
   return (
     <div className='post'>
-        <h3 className='post__title'>post title</h3>
+        <h3 className='post__title'>{props.title}</h3>
+        <h4 className='post__body'>{props.content}</h4>
     </div>
   );
 };

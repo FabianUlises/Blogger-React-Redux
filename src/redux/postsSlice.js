f7c60f115b3f44c6b4d9d6cae3cf26c676d4108b
FabianUlises <ulisesfab.car@outlook.com>
2023-04-07 21:01:18 -0700

    Created reducer for redux

--- src/redux/postsSlice.js
+++ src/redux/postsSlice.js
@@ -12,3 +12,24 @@ export const fetchPosts = createAsyncThunk('posts/fetch', async() => {
     const res = await axios.get(apiURL);
     return res.data;
 });
+const postsSlice = createSlice({
+    name: 'posts',
+    initialState,
+    extraReducers: (builder) => {
+        builder.addCase(fetchPosts.pending, (state, action) => {
+            state.loading = true;
+        });
+        builder.addCase(fetchPosts.fulfilled, (state, action) => {
+            state.loading = false;
+            state.posts = action.payload;
+
+        });
+        builder.addCase(fetchPosts.rejected, (state, action) => {
+            state.loading = false;
+            state.posts = [];
+            state.error = action.payload;
+        });
+    }
+});
+const postReducer = postsSlice.reducer;
+export default postReducer;

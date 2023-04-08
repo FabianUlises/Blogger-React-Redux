c3ef31dc4c1c346a3848e282fabd414bfdaa6cca
FabianUlises <ulisesfab.car@outlook.com>
2023-04-07 17:57:12 -0700

    Created post component to display fetched data

---
+++ src/components/post/Post.js
@@ -0,0 +1,11 @@
+// Dependencies
+import React from 'react';
+const Post = () => {
+  return (
+    <div className='post'>
+        <h3 className='post__title'>posttitle</h3>
+    </div>
+  );
+};
+
+export default Post;

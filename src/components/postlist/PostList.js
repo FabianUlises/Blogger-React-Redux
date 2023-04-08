da9ac7f6af81fb2211f0820644af94c19673de0e
FabianUlises <ulisesfab.car@outlook.com>
2023-04-07 17:55:40 -0700

    Created postlist component to contain single post

---
+++ src/components/postlist/PostList.js
@@ -0,0 +1,14 @@
+// Dependencies
+import React from 'react';
+const PostsList = () => {
+  return (
+    <section className='postlist-container'>
+      <h2 className='section-title'>Total Posts 100</h2>
+      <div className='posts-container'>
+        <h2>postlist</h2>
+      </div>
+    </section>
+  );
+};
+
+export default PostsList;

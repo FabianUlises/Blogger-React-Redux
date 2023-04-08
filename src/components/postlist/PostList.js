a84d8bf8d919e2922510c66f3dca701a6c4200a9
FabianUlises <ulisesfab.car@outlook.com>
2023-04-07 21:42:49 -0700

    Getting data from redux store

--- src/components/postlist/PostList.js
+++ src/components/postlist/PostList.js
@@ -1,10 +1,18 @@
 // Dependencies
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 // Components
 import Post from './../post/Post';
 // Styles
 import styles from './postlist.css';
+// Redux actions
+import { fetchPosts } from '../../redux/slice/postsSlice';
 const PostsList = () => {
+  const dispatch = useDispatch();
+  // Useeffect
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, []);
   return (
     <section className='postlist-container'>
       <h2 className='section-title'>Total Posts 100</h2>
